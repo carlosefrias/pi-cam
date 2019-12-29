a97738210abeea9cf2cd2794d3c82be42858e9e9
Carlos Frias <carlosefrias@gmail.com>
2019-12-29 00:00:17 +0000

    cpu improvements: based on https://github.com/miguelgrinberg/flask-video-streaming/tree/v1

--- webstreaming.py
+++ webstreaming.py
@@ -1,94 +1,32 @@
-from imutils.video import VideoStream
-import imutils
-from flask import Response, Flask, render_template
-import threading
-import argparse
-import numpy as np
-import datetime
-import time
-import cv2.cv2 as cv2
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response
 
+from camera_opencv import Camera
 
-outputFrame = None
-lock = threading.Lock()
 
 app = Flask(__name__)
 
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
 
-@app.route("/")
+@app.route('/')
 def index():
-    # return the rendered template
-    return render_template("index.html")
+    """Video streaming home page."""
+    return render_template('index.html')
 
-	
-def detect_motion(frameCount):
-    global vs, outputFrame, lock
+
+def gen(camera):
+    """Video streaming generator function."""
     while True:
-        frame = vs.read()
-        frame = imutils.resize(frame, width=600, height=400)
-        timestamp = datetime.datetime.now()
-        timestamp = datetime.datetime.now()
-        cv2.putText(frame, timestamp.strftime(
-            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
-        with lock:
-            outputFrame = frame.copy()
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-def generate():
-	# grab global references to the output frame and lock variables
-	global outputFrame, lock
- 
-	# loop over frames from the output stream
-	while True:
-		# wait until the lock is acquired
-		with lock:
-			# check if the output frame is available, otherwise skip
-			# the iteration of the loop
-			if outputFrame is None:
-				continue
- 
-			# encode the frame in JPEG format
-			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
- 
-			# ensure the frame was successfully encoded
-			if not flag:
-				continue
- 
-		# yield the output frame in the byte format
-		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
-			bytearray(encodedImage) + b'\r\n')
 
-@app.route("/video_feed")
+@app.route('/video_feed')
 def video_feed():
-	# return the response generated along with the specific media
-	# type (mime type)
-	return Response(generate(),
-		mimetype = "multipart/x-mixed-replace; boundary=frame")
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-# check to see if this is the main thread of execution
 if __name__ == '__main__':
-	# construct the argument parser and parse command line arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--ip", type=str, required=True,
-		help="ip address of the device")
-	ap.add_argument("-o", "--port", type=int, required=True,
-		help="ephemeral port number of the server (1024 to 65535)")
-	ap.add_argument("-f", "--frame-count", type=int, default=32,
-		help="# of frames used to construct the background model")
-	args = vars(ap.parse_args())
- 
-	# start a thread that will perform motion detection
-	t = threading.Thread(target=detect_motion, args=(
-		args["frame_count"],))
-	t.daemon = True
-	t.start()
- 
-	# start the flask app
-	app.run(host=args["ip"], port=args["port"], debug=True,
-		threaded=True, use_reloader=False)
- 
-# release the video stream pointer
-vs.stop()
+    app.run(host='0.0.0.0', port= 7024, threaded=True)
